e629335189ec4658a74dbebc2cf6bd632653e15c
MJonny4 <ion2000m@gmail.com>
2023-08-08 11:36:13 +0200

    Need some touches

---
+++ Readme.rst
@@ -0,0 +1,5 @@
+Authors:
+    Ion M
+
+:Version: 1.0 of 08/08/2023
+:Difficulty: Medium
---
+++ src/context/mockData.js/mockFollowers.jsx
@@ -0,0 +1,121 @@
+export default [
+    {
+        login: "Oblivios",
+        id: 68814228,
+        node_id: "MDQ6VXNlcjY4ODE0MjI4",
+        avatar_url: "https://avatars.githubusercontent.com/u/68814228?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/Oblivios",
+        html_url: "https://github.com/Oblivios",
+        followers_url: "https://api.github.com/users/Oblivios/followers",
+        following_url:
+            "https://api.github.com/users/Oblivios/following{/other_user}",
+        gists_url: "https://api.github.com/users/Oblivios/gists{/gist_id}",
+        starred_url:
+            "https://api.github.com/users/Oblivios/starred{/owner}{/repo}",
+        subscriptions_url:
+            "https://api.github.com/users/Oblivios/subscriptions",
+        organizations_url: "https://api.github.com/users/Oblivios/orgs",
+        repos_url: "https://api.github.com/users/Oblivios/repos",
+        events_url: "https://api.github.com/users/Oblivios/events{/privacy}",
+        received_events_url:
+            "https://api.github.com/users/Oblivios/received_events",
+        type: "User",
+        site_admin: false,
+    },
+    {
+        login: "Eraixs",
+        id: 95909199,
+        node_id: "U_kgDOBbd1Tw",
+        avatar_url: "https://avatars.githubusercontent.com/u/95909199?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/Eraixs",
+        html_url: "https://github.com/Eraixs",
+        followers_url: "https://api.github.com/users/Eraixs/followers",
+        following_url:
+            "https://api.github.com/users/Eraixs/following{/other_user}",
+        gists_url: "https://api.github.com/users/Eraixs/gists{/gist_id}",
+        starred_url:
+            "https://api.github.com/users/Eraixs/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/Eraixs/subscriptions",
+        organizations_url: "https://api.github.com/users/Eraixs/orgs",
+        repos_url: "https://api.github.com/users/Eraixs/repos",
+        events_url: "https://api.github.com/users/Eraixs/events{/privacy}",
+        received_events_url:
+            "https://api.github.com/users/Eraixs/received_events",
+        type: "User",
+        site_admin: false,
+    },
+    {
+        login: "alyayee",
+        id: 96149189,
+        node_id: "U_kgDOBbsexQ",
+        avatar_url: "https://avatars.githubusercontent.com/u/96149189?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/alyayee",
+        html_url: "https://github.com/alyayee",
+        followers_url: "https://api.github.com/users/alyayee/followers",
+        following_url:
+            "https://api.github.com/users/alyayee/following{/other_user}",
+        gists_url: "https://api.github.com/users/alyayee/gists{/gist_id}",
+        starred_url:
+            "https://api.github.com/users/alyayee/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/alyayee/subscriptions",
+        organizations_url: "https://api.github.com/users/alyayee/orgs",
+        repos_url: "https://api.github.com/users/alyayee/repos",
+        events_url: "https://api.github.com/users/alyayee/events{/privacy}",
+        received_events_url:
+            "https://api.github.com/users/alyayee/received_events",
+        type: "User",
+        site_admin: false,
+    },
+    {
+        login: "eduardtomas1",
+        id: 110898623,
+        node_id: "U_kgDOBpwtvw",
+        avatar_url: "https://avatars.githubusercontent.com/u/110898623?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/eduardtomas1",
+        html_url: "https://github.com/eduardtomas1",
+        followers_url: "https://api.github.com/users/eduardtomas1/followers",
+        following_url:
+            "https://api.github.com/users/eduardtomas1/following{/other_user}",
+        gists_url: "https://api.github.com/users/eduardtomas1/gists{/gist_id}",
+        starred_url:
+            "https://api.github.com/users/eduardtomas1/starred{/owner}{/repo}",
+        subscriptions_url:
+            "https://api.github.com/users/eduardtomas1/subscriptions",
+        organizations_url: "https://api.github.com/users/eduardtomas1/orgs",
+        repos_url: "https://api.github.com/users/eduardtomas1/repos",
+        events_url:
+            "https://api.github.com/users/eduardtomas1/events{/privacy}",
+        received_events_url:
+            "https://api.github.com/users/eduardtomas1/received_events",
+        type: "User",
+        site_admin: false,
+    },
+    {
+        login: "AlyssaPuype",
+        id: 115114541,
+        node_id: "U_kgDOBtyCLQ",
+        avatar_url: "https://avatars.githubusercontent.com/u/115114541?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/AlyssaPuype",
+        html_url: "https://github.com/AlyssaPuype",
+        followers_url: "https://api.github.com/users/AlyssaPuype/followers",
+        following_url:
+            "https://api.github.com/users/AlyssaPuype/following{/other_user}",
+        gists_url: "https://api.github.com/users/AlyssaPuype/gists{/gist_id}",
+        starred_url:
+            "https://api.github.com/users/AlyssaPuype/starred{/owner}{/repo}",
+        subscriptions_url:
+            "https://api.github.com/users/AlyssaPuype/subscriptions",
+        organizations_url: "https://api.github.com/users/AlyssaPuype/orgs",
+        repos_url: "https://api.github.com/users/AlyssaPuype/repos",
+        events_url: "https://api.github.com/users/AlyssaPuype/events{/privacy}",
+        received_events_url:
+            "https://api.github.com/users/AlyssaPuype/received_events",
+        type: "User",
+        site_admin: false,
+    },
+];
